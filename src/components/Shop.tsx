f0985f24bd1fc525222730f5cc629aeb2c34f5c4
Choring <skyhonor201@gmail.com>
2024-12-18 00:25:09 +0900

    타입 수정

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,5 +1,6 @@
+import { ReactNode } from "react"
 
-export const Shop = ({ children }) => {
+export const Shop = ({ children }:{ children:ReactNode }) => {
   return (
     <section id="shop">
         <h2>모두를 위한 세련된 패션</h2>
