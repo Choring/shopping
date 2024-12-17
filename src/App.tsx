861d8dcbc35873a95af44dd5adef20b8ff5dcc2a
Choring <skyhonor201@gmail.com>
2024-12-17 13:40:08 +0900

    쇼핑몰 프로젝트 기본세팅

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Header } from "./components/Layout/Header"
+import { Footer } from "./components/Layout/Footer"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h2>Hello React + Vite + TypeScript</h2>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Tailwind가 스캔할 파일 경로 지정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
