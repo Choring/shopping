dcd822ec58f422c1f821b1ad5f7d820a21ee8047
Choring <skyhonor201@gmail.com>
2024-12-17 14:18:39 +0900

    장바구니 및 장바구니 업데이트 함수 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,78 @@
 import './App.css'
-import { Header } from "./components/Layout/Header"
-import { Footer } from "./components/Layout/Footer"
+import { Header } from "./components/Layout/Header";
+import { Footer } from "./components/Layout/Footer";
+import { Shop } from "./components/Shop";
+import { useState } from "react";
+import { DUMMY_PRODUCTS } from "./components/dummy-products";
 
 function App() {
+  const [shoppingCart, setShoppingCart] = useState<ShoppingCart>({
+    items: []
+  })
+  
+  const handleAddItemToCart = (id:string) => {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product?.title || "",
+          price: product?.price || 0,
+          quantity: 1,
+        });
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  const handleUpdateCartItemQuantity = (productId:string, amount:number) => {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
 
   return (
     <>
-      <Header />
-      <h2>Hello React + Vite + TypeScript</h2>
+      <Header
+        cart={shoppingCart}
+        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+      />
+      <Shop onAddItemToCart={handleAddItemToCart}/>
       <Footer />
     </>
   )
