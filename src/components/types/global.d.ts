f6a226dec73391e0470e51c9aba6a5fa6f0d86a1
Choring <skyhonor201@gmail.com>
2024-12-17 14:17:40 +0900

    타입 수정

--- src/components/types/global.d.ts
+++ src/components/types/global.d.ts
@@ -3,6 +3,17 @@ export {}
 declare global {
     // interface 이름 {} or type 이름 = {}
 
+    type ShoppingCartItem = {
+        id: string,
+        name: string,
+        price: number,
+        quantity: number
+
+    }
+    interface ShoppingCart {
+        items: ShoppingCartItem[]
+    }
+
     type DummyProducts = {
         id: string;
         image: string;
@@ -10,4 +21,17 @@ declare global {
         price: number;
         description: string;
     }[]
+
+    interface ShopProps {
+        onAddItemToCart: (id:string) => void;
+    }
+
+    interface ProductProps {    
+        onAddToCart: () => void;
+    }
+
+    interface HeaderProps {
+        cart: ShoppingCart;
+        onUpdateCartItemQuantity: (productId:string, amount:number) => void;
+    }
 }
