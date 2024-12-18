8a12b5c168767d2e72dbc93cc9a1286ff894eab7
조인철 <skyhonor201@gmail.com>
2024-12-18 19:44:35 +0900

    더미데이터 한글 번역

--- src/dummy-products.ts
+++ src/dummy-products.ts
@@ -9,50 +9,50 @@ export const DUMMY_PRODUCTS: DummyProducts = [
     {
       id: 'p1',
       image: mochaOvercoat,
-      title: 'Majestic Vintage Mocha Overcoat',
+      title: '고급스러운 빈티지 모카 오버코트',
       price: 129.99,
       description:
-        'Channel timeless sophistication with this stunning mocha overcoat. Crafted for the discerning gentleman who appreciates the fine blend of vintage charm and modern elegance.',
+        '이 놀라운 모카 오버코트로 시간을 초월한 세련미를 연출하세요. 빈티지 감성과 현대적인 우아함의 조화를 소중히 여기는 안목 있는 신사를 위해 제작되었습니다.',
     },
     {
       id: 'p2',
       image: dreamGown,
-      title: 'Enchanting Blush Dream Gown',
+      title: '황홀한 블러쉬 드림 드레스',
       price: 189.99,
       description:
-        'Bask in the glow of elegance with our Enchanting Blush Dream Gown. Embody the regality of a queen with a sweet touch of pink that whispers enchantment. This is the perfect piece for those seeking to create unforgettable moments.',
+        '매혹적인 블러쉬 드림 가운과 함께 우아함의 빛을 만끽하세요. 달콤한 핑크의 매혹적인 속삭임으로 여왕의 품격을 담아냅니다. 잊지 못할 순간을 만들고자 하는 이들을 위한 완벽한 선택입니다',
     },
   
     {
       id: 'p3',
       image: rainJacket,
-      title: 'Vintage Sunshine Rain Jacket',
+      title: '빈티지 감성의 선샤인 레인 재킷',
       price: 49.99,
       description:
-        'Brace the showers in style! Our Vintage Sunshine Rain Jacket ensures that you stand out, even in the dullest weather. Because rain is never a reason to compromise on your fashion quotient.',
+        '스타일리시하게 비를 맞이하세요! 빈티지 선샤인 레인 재킷은 가장 흐린 날씨 속에서도 돋보이게 해줍니다. 비가 온다고 해서 패션을 포기할 이유는 없으니까요.',
     },
     {
       id: 'p4',
       image: merlotSuit,
-      title: 'Classic Merlot Business Suit',
+      title: '고급스러운 클래식 멀로 수트',
       price: 249.99,
       description:
-      'Step into the boardroom with unmatched confidence in our Classic Merlot Business Suit. Exuding an air of refined class and understated power, it is ideal for the modern executive who values tradition and elegance.',
+      '클래식 멀로 비즈니스 수트와 함께 비즈니스 룸에 자신감을 더하세요. 세련된 품격과 절제된 힘을 발산하는 이 수트는 전통과 우아함을 중시하는 현대적인 임원에게 이상적입니다.',
       },
       {
       id: 'p5',
       image: moonlightDress,
-      title: 'Ethereal Moonlight Evening Dress',
+      title: '몽환적인 달빛 이브닝 드레스',
       price: 159.99,
       description:
-      'Sweep the room off its feet in our Ethereal Moonlight Evening Dress. Crafted to mimic the allure of the moonlight, this dress is a nod to those who appreciate subtle glamour and a standout silhouette.',
+      '천상의 달빛 이브닝 드레스로 방 안을 압도하세요. 달빛의 매력을 그대로 담아낸 이 드레스는 섬세한 매력과 돋보이는 실루엣을 사랑하는 이들을 위한 선택입니다.',
       },
       {
       id: 'p6',
       image: denimPioneer,
-      title: 'Pioneer Rugged Denim Jacket',
+      title: '강인한 파이오니어 러기드 데님 재킷',
       price: 79.99,
       description:
-      'Our Pioneer Rugged Denim Jacket is a tribute to those who embody the spirit of adventure. Designed with durability and comfort in mind, this jacket is a wardrobe essential for the urban explorer.'
+      '우리의 파이오니어 러기드 데님 재킷은 모험 정신을 몸소 실천하는 이들을 위한 헌사입니다. 내구성과 편안함을 고려하여 디자인된 이 재킷은 도시 탐험가에게 필수적인 아이템입니다.'
       }
   ];
